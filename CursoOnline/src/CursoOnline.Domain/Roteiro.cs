442ce81006ddf4ef9910374fe8822ae9a4013a98
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2021-04-13 06:23:31 -0300

    Initial commit

---
+++ CursoOnline/src/CursoOnline.Domain/Roteiro.cs
@@ -0,0 +1,15 @@
+﻿namespace CursoOnline.Domain
+{
+    class Roteiro
+    {
+        /*
+         
+        Critérios de aceitação
+            Preciso de um sistema que administre os cursos onlines, pra isso preciso criar e editar para esses cursos antes da abertura de matriculas.
+                - Criar um curso com nome, carga horária, publico alvo e valor do curso
+                - As opções para o público alvo são: Estudante, Universitário, Empregado e Empreendedor.
+                - Todos os campos do Curso são obrigatórios.
+        
+         */
+    }
+}
---
+++ CursoOnline/test/CursoOnline.Domain.Test/Cursos/CursosTets.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoOnline.Domain.Test.Cursos
+{
+    class CursosTets
+    {
+    }
+}
